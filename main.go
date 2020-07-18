11503923fc794eb98eb52096833d6d1f20f048ac
wahiddinishak <ishakwahiddin@gmamil.com>
2020-07-19 03:45:14 +0700

    create route add-mobil

--- main.go
+++ main.go
@@ -7,14 +7,16 @@ import (
 	"os"
 )
 
+// Class data mobil (melakukan pemodelan data moobil dengan member(info detail) ID, Merk, dan Tahun)
 type mobil struct {
 	ID    int    `json:"id"`
 	Merk  string `json:"merk"`
 	Tahun int    `json:"tahun"`
 }
 
+// Variable database in memory "sementara"
 var (
-	database = make(map[string]mobil)
+	database = make(map[int]mobil)
 )
 
 // JSON Respon
@@ -27,32 +29,30 @@ func setResp(res http.ResponseWriter, msg []byte, httpCode int) {
 func main() {
 
 	// init database
-	database["1"] = mobil{ID: 1, Merk: "Honda Jazz", Tahun: 2020}
-	database["2"] = mobil{ID: 2, Merk: "Toyota Avanza", Tahun: 2019}
-	database["3"] = mobil{ID: 3, Merk: "Suzuki Baleno", Tahun: 2007}
+	database[1] = mobil{ID: 1, Merk: "Honda Jazz", Tahun: 2020}
+	database[2] = mobil{ID: 2, Merk: "Toyota Avanza", Tahun: 2019}
+	database[3] = mobil{ID: 3, Merk: "Suzuki Baleno", Tahun: 2007}
 
-	// main route
+	// main route (route utama)
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		msg := []byte(`"{message}" : "Server is up!"`)
 		setResp(res, msg, http.StatusOK)
 	})
 
-	// route get data mobil
+	// route get data mobil (pemanggilan seluruh data mobil)
 	http.HandleFunc("/get-mobils", func(res http.ResponseWriter, req *http.Request) {
+		// Validasi method
 		if req.Method != "GET" {
 			msg := []byte(`"{message}" : "invalid http method"`)
 			setResp(res, msg, http.StatusOK)
 			return
 		}
-
 		// temp list class data mobil
 		var mobils []mobil
-
 		// loop data mobil into mobils
 		for _, mobil := range database {
 			mobils = append(mobils, mobil)
 		}
-
 		// encode to JSON if not error
 		JSONmobils, err := json.Marshal(&mobils)
 		if err != nil {
@@ -60,10 +60,34 @@ func main() {
 			setResp(res, msg, http.StatusInternalServerError)
 			return
 		}
-
 		setResp(res, JSONmobils, http.StatusOK)
 	})
 
+	// route add mobil (menambahkan data mobil ke database)
+	http.HandleFunc("/add-mobil", func(res http.ResponseWriter, req *http.Request) {
+		// Validasi Method
+		if req.Method != "POST" {
+			msg := []byte(`"{message}" : "invalid http method"`)
+			setResp(res, msg, http.StatusOK)
+			return
+		}
+		var mobilx mobil                                // variable class mobil
+		payload := req.Body                             // mengambil/req payload yang dikirim dari sisi client
+		defer req.Body.Close()                          // menutup req
+		err := json.NewDecoder(payload).Decode(&mobilx) // melakukan deserialize / decode dari payload ke item mobilx
+		// Validasi jika data error / tidak terisi misalnya
+		if err != nil {
+			msg := []byte(`"{message}" : "error when parsing data"`)
+			setResp(res, msg, http.StatusInternalServerError)
+			return
+		}
+		// menambahkan data dari moobilx ke database dengan ID (sesuai format map)
+		database[mobilx.ID] = mobilx
+		msg := []byte(`"{message}" : "success create data mobil"`)
+		setResp(res, msg, http.StatusOK)
+	})
+
+	// Config port
 	err := http.ListenAndServe(":65", nil)
 	if err != nil {
 		fmt.Print(err)
